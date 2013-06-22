acd2e0222a836310233a5e1facfa295ea74a3ad4
Evan Allan <evanallan1026@gmail.com>
2013-06-22 19:19:49 -0400

    Problem 6 done

---
+++ src/problem6.cpp
@@ -0,0 +1,13 @@
+#include  <iostream>
+using namespace std;
+
+int main(){
+    long sum       = 0,
+         squareSum = 0;
+
+    for (int i = 1; i <= 100; i++){
+        sum += i;
+        squareSum += (i * i);
+    }
+    cout << (sum * sum - squareSum) << endl;
+}
