22ab22140b673b8c5de645e827d38d6678ec28ae
Evan Allan <evanallan1026@gmail.com>
2013-06-22 14:45:55 -0400

    Problem 1 done

--- problem1.cpp
+++ problem1.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-
+#include <sstream>
+using namespace std;
 int main(){
-    
+    cout << "Max number: ";
+
+    int maxNum = 0,
+        sum    = 0;
+    string input;
+
+    cin >> input;
+    if(!(stringstream(input) >> maxNum)){
+        cout << "Invalid number" << endl;
+        return(0);
+    }
+
+    for (int i = 0; i < maxNum; i++){
+        if ((i % 3 == 0) || (i % 5 == 0)){
+            sum += i;
+        }
+    }
+
+    cout << sum << endl;
 }
