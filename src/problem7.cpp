89cc0de7ad306794e2f918255dd882bfb59ba191
Evan Allan <evanallan1026@gmail.com>
2013-06-22 19:25:28 -0400

    Problem 7 done

---
+++ src/problem7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num);
+
+int main(){
+    int prime     = 0,
+        numPrimes = 0;
+
+    for (int i = 2; numPrimes <= 10001; i++){
+        if (isPrime(i)){
+            prime = i;
+            numPrimes++;
+        }
+    }
+
+    cout << prime << endl;
+}
+
+bool isPrime(int num){
+    for (int i = 2; i < num / 2; i += 1){
+        if (num % i == 0) return false;
+    }
+    return true;
+}
