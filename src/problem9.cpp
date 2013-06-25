ba93443b9e56536dd53954799c84d74e7b893a6e
Evan Allan <evanallan1026@gmail.com>
2013-06-25 19:50:10 -0400

    Problem 9 done

---
+++ src/problem9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    for (int a = 1; a < 1000; a++){
+        for (int b = 1; b < 1000; b++){
+            double c = sqrt((a * a) + (b * b));
+            if (a + b + c == 1000) {
+                cout << (a * b * c) << endl;
+                return 0;
+            }
+        }
+    }
+}
