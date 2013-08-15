e1f147619c1b6f345ed6dfc16c5dcfabe4be60f0
Evan Allan <evanallan1026@gmail.com>
2013-08-14 22:58:57 -0400

    Problem 19 done. I've decided to use whatever language I want in this, defaulting to c++ when I don't really care. Take that Obama!

---
+++ src/problem19.py
@@ -0,0 +1,17 @@
+import datetime
+
+startDay = datetime.date(1901, 1, 1)
+endDay = datetime.date(2000, 12, 31)
+currDay = startDay
+count = 0
+
+while (currDay < endDay):
+    if (currDay.month < 12):
+        currDay = currDay.replace(month = (currDay.month + 1))
+    else:
+        currDay = currDay.replace(month = 1, year = currDay.year + 1)
+
+    if (currDay.weekday() == 6):
+        count += 1
+
+print count
